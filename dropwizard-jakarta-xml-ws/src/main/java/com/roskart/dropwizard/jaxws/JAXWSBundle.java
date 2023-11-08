e8173cd2dfc77aa4224b2d84ca3e9e99674beb05
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-11-08 16:18:43 -0500

    Remove deprecated methods from JAXWSBundle (#66)
    Closes #65

--- dropwizard-jakarta-xml-ws/src/main/java/com/roskart/dropwizard/jaxws/JAXWSBundle.java
+++ dropwizard-jakarta-xml-ws/src/main/java/com/roskart/dropwizard/jaxws/JAXWSBundle.java
@@ -5,10 +5,8 @@ import static com.google.common.base.Preconditions.checkArgument;
 import io.dropwizard.core.ConfiguredBundle;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
-import jakarta.xml.ws.handler.Handler;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import org.hibernate.SessionFactory;
 
 /**
  * A Dropwizard bundle that enables Dropwizard applications to publish SOAP web services using
@@ -81,92 +79,6 @@ public class JAXWSBundle<C> implements ConfiguredBundle<C> {
     public EndpointImpl publishEndpoint(EndpointBuilder endpointBuilder) {
         checkArgument(endpointBuilder != null, "EndpointBuilder is null");
         return this.jaxwsEnvironment.publishEndpoint(endpointBuilder);
-    }
-
-    /**
-     * Publish Jakarta XML Web Services endpoint. Endpoint is published relative to the CXF servlet path.
-     *
-     * @param path    Relative endpoint path.
-     * @param service Service implementation.
-     * @return javax.xml.ws.Endpoint
-     * @deprecated Use the {@link #publishEndpoint(EndpointBuilder) publishEndpoint} method instead.
-     */
-    @Deprecated(since = "0.5.0")
-    public EndpointImpl publishEndpoint(String path, Object service) {
-        return this.publishEndpoint(path, service, null, null);
-    }
-
-    /**
-     * Publish Jakarta XML Web Services endpoint with Dropwizard Hibernate Bundle integration. Service is
-     * scanned for @UnitOfWork annotations. EndpointBuilder is published relative to the CXF servlet path.
-     *
-     * @param path           Relative endpoint path.
-     * @param service        Service implementation.
-     * @param sessionFactory Hibernate session factory.
-     * @return javax.xml.ws.Endpoint
-     * @deprecated Use the {@link #publishEndpoint(EndpointBuilder) publishEndpoint} method instead.
-     */
-    @Deprecated(since = "0.5.0")
-    public EndpointImpl publishEndpoint(String path, Object service, SessionFactory sessionFactory) {
-        return this.publishEndpoint(path, service, null, sessionFactory);
-    }
-
-    /**
-     * Publish Jakarta XML Web Services protected endpoint using Dropwizard BasicAuthentication.
-     * EndpointBuilder is published relative to the CXF servlet path.
-     *
-     * @param path           Relative endpoint path.
-     * @param service        Service implementation.
-     * @param authentication BasicAuthentication implementation.
-     * @return javax.xml.ws.Endpoint
-     * @deprecated Use the {@link #publishEndpoint(EndpointBuilder) publishEndpoint} method instead.
-     */
-    @Deprecated(since = "0.5.0")
-    public EndpointImpl publishEndpoint(String path, Object service, BasicAuthentication authentication) {
-        return this.publishEndpoint(path, service, authentication, null);
-    }
-
-    /**
-     * Publish Jakarta XML Web Services protected endpoint using Dropwizard BasicAuthentication with
-     * Dropwizard Hibernate Bundle integration. Service is scanned for @UnitOfWork annotations. EndpointBuilder
-     * is published relative to the CXF servlet path.
-     *
-     * @param path           Relative endpoint path.
-     * @param service        Service implementation.
-     * @param auth           BasicAuthentication implementation.
-     * @param sessionFactory Hibernate session factory.
-     * @return javax.xml.ws.Endpoint
-     * @deprecated Use the {@link #publishEndpoint(EndpointBuilder) publishEndpoint} method instead.
-     */
-    @Deprecated(since = "0.5.0")
-    public EndpointImpl publishEndpoint(String path, Object service, BasicAuthentication auth,
-                                        SessionFactory sessionFactory) {
-        checkArgument(service != null, "Service is null");
-        checkArgument(path != null, "Path is null");
-        checkArgument((path).trim().length() > 0, "Path is empty");
-        return this.publishEndpoint(new EndpointBuilder(path, service)
-                .authentication(auth)
-                .sessionFactory(sessionFactory)
-        );
-    }
-
-    /**
-     * Factory method for creating Jakarta XML Web Services clients.
-     *
-     * @param serviceClass Service interface class.
-     * @param address      Endpoint URL address.
-     * @param handlers     Client side Jakarta XML Web Services handlers. Optional.
-     * @param <T>          Service interface type.
-     * @return Jakarta XML Web Services client proxy.
-     * @deprecated Use the {@link #getClient(ClientBuilder) getClient} method instead.
-     */
-    @Deprecated(since = "0.5.0")
-    public <T> T getClient(Class<T> serviceClass, String address, Handler... handlers) {
-        checkArgument(serviceClass != null, "ServiceClass is null");
-        checkArgument(address != null, "Address is null");
-        checkArgument((address).trim().length() > 0, "Address is empty");
-        return jaxwsEnvironment.getClient(
-                new ClientBuilder<>(serviceClass, address).handlers(handlers));
     }
 
     /**
